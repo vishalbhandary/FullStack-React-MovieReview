606912a9c657d47ce739f5d355f8170af6a3cc88
Vishal SB <vishalbhandary18@gmail.com>
2024-09-25 18:07:57 +0530

    Finished(3rd time adding this to repo)
    This is an review application for movies where user can browse through movies, watch trailers of the movie, and also give reviews about it.
    This application is built using Java Spring Boot as Backend Technology and React as Frontend.

---
+++ movies/src/main/java/com/vishal/movies/ReviewController.java
@@ -0,0 +1,26 @@
+package com.vishal.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+    @Autowired
+    private ReviewService service;
+
+    @PostMapping()
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        Review createdReview = service.createReview(
+                payload.get("reviewBody"),
+                payload.get("imdbId")
+        );
+        return new ResponseEntity<>(createdReview, HttpStatus.CREATED);
+    }
+}
